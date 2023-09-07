fa83e78eef763cff4508ccef73dc4a7a88e76909
derickson <david.erickson@gmail.com>
2023-09-07 14:51:28 -0400

    nav

--- main_page.py
+++ main_page.py
@@ -5,9 +5,14 @@ st.set_page_config(layout="wide")
 st.markdown("# Streamlit Elasticsearch and LangChain Examples 🎈")
 
 
-st.markdown('<a href="/langchain_clueless" target="_self">LangChain Clueless</a>', unsafe_allow_html=True)
-st.markdown('<a href="/🦙_llama2" target="_self">Llama2</a>', unsafe_allow_html=True)
-st.markdown('<a href="/ELSER_NSF_search" target="_self">ELSER NSF Search</a>', unsafe_allow_html=True)
+st.markdown(''' 
+## Basic Chat Bots
+* <a href="/langchain_clueless" target="_self">LangChain Azure OpenAI Clueless</a>
+* <a href="/%F0%9F%87%BB_google_vertex" target="_self">Google Vertex Chat</a>
+* <a href="/🦙_llama2" target="_self">Llama2 Chat</a>
+## Semantic Search
+* <a href="/ELSER_NSF_search" target="_self">ELSER NSF Search</a>
+''', unsafe_allow_html=True)
 
 
 
